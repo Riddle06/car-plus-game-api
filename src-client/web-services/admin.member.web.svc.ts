66e2fd8d094a42083796b8d418e926b53c264fc7
Rex Li <rex@myre.life>
2018-11-18 20:41:50 +0800

    feat:  新增 admin member web service 說明

--- src-client/web-services/admin.member.web.svc.ts
+++ src-client/web-services/admin.member.web.svc.ts
@@ -2,37 +2,56 @@ import { Result } from '@view-models/common.vm';
 import { BaseWebSvc } from "./base-web.svc";
 export class AdminMemberWebSvc extends BaseWebSvc {
 
+    /**
+     * 取得遊戲紀錄
+     */
     getGameHistories() {
 
     }
+    /**
+     * 手動新增遊戲幣
+     */
     addGamePoint() {
 
     }
 
+    /**
+     * 取得新增遊戲幣紀錄
+     */
     getManualGamePointHistories() {
 
     }
 
-    getGamePointHistories() {
+    /**
+     * 取得紅利兌換紀錄
+     */
+    getGamePointExchangeHistories() {
 
     }
 
+    /**
+     * 新增黑名單
+     */
     blockMember() {
 
     }
 
+    /**
+     * 移除黑名單
+     */
     unblockMember() {
 
     }
 
-    getMemberList() {
-
-    }
-
+    /**
+     * 取得黑名單列表
+     */
     getBlockMember() {
 
     }
-
+    /**
+     * 取得消費者及所屬商品數量
+     */
     getMemberWithGameItems() { 
         
     }
