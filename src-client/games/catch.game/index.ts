3cd5e75f48f6779c0adef4ef0612f204902a0f30
david.jian <clipwww@gmail.com>
2018-11-18 15:09:02 +0800

    feat: [接東西] 羊駝動起來

--- src-client/games/catch.game/index.ts
+++ src-client/games/catch.game/index.ts
@@ -1,13 +1,68 @@
-import { BaseGame } from "../base.game";
+import { BaseGame, hitTestRectangle } from "../base.game";
+import { Graphics } from "pixi.js";
+import { SuperMan, SuperManDirection } from './super-man';
+import { FallItem } from './fall-item';
+// import * as dat from "dat.gui";
+// const gui = new dat.GUI();
 
 export class CatchGame extends BaseGame {
-    protected initElements(): Promise<boolean> {
-        throw new Error("Method not implemented.");
+    private superMan: SuperMan = null;
+    private fallItems: PIXI.Graphics[] = [];
+
+    protected async initElements(): Promise<boolean> {
+
+        // 建立遊戲
+        document.querySelector("#app-game").appendChild(this.application.view);
+        // 建立超人
+        this.superMan = await new SuperMan(this.application).init();
+        // 建立掉落物品
+        setInterval(() => {
+            // 每秒建立一次
+            this.generateFallItemHandler()
+        }, 1000);
+        
+        return Promise.resolve(true);
     }
     protected initElementsOffset(): Promise<boolean> {
-        throw new Error("Method not implemented.");
+        return Promise.resolve(true);
     }
     protected initElementsEvents(): Promise<boolean> {
-        throw new Error("Method not implemented.");
+        
+        document.addEventListener('pointerdown', (e) => {
+            // 點擊畫面事件
+            console.log('[pointerdown]', e)
+            this.superMan.turnDirection();
+        });
+
+        this.application.ticker.add(this.handleHitItem.bind(this));
+
+        return Promise.resolve(true);
+    }
+
+    private async generateFallItemHandler(): Promise<void> {
+
+        // 每次產生 0~3個掉落物
+        const generateItems = Math.floor(Math.random() * 3);
+        for (let i = 0; i < generateItems; i++) {
+            const shape = await new FallItem(this.application).init();
+            this.fallItems.push(shape);
+            this.application.stage.addChild(shape)
+        }
+
+    }
+
+    private handleHitItem() {
+        this.fallItems.filter(item => hitTestRectangle(this.superMan.sprite, item)).forEach(item => {
+            // 檢查掉落物與超人是否碰撞，是 -> 隱藏+移除
+            item.visible = false
+            item.removeChild(item);
+        })
+        this.fallItems.filter(item => item.y > this.application.screen.height).forEach(item => {
+            // 檢查掉落物是否掉出畫面了，是 -> 隱藏+移除
+            item.visible = false
+            item.removeChild(item);
+        })
+
+        this.fallItems = this.fallItems.filter(item => item.visible)
     }
 }
--- src-client/games/catch.game/super-man.ts
+++ src-client/games/catch.game/super-man.ts
@@ -1,21 +1,37 @@
 import * as PIXI from "pixi.js";
+import { loaderHandler } from '../base.game';
 export class SuperMan {
     private initPosition: {
         x: number
         y: number
     } = null
-    private moveSpeed: number = 1
-    private sprite: PIXI.extras.AnimatedSprite = null;
+    private moveSpeed: number = 3
     private app: PIXI.Application = null
-
     private currentDirection: SuperManDirection = SuperManDirection.right
+    
+    public sprite: PIXI.extras.AnimatedSprite = null;
+    
     constructor(app: PIXI.Application) {
         this.app = app;
     }
 
     async init(): Promise<this> {
-        this.app.ticker.add(this.movingHandler)
-        this.app.ticker.add(this.flipDirectionHandler)
+        const loderRes = await loaderHandler('/static/images/llama/llama.json');
+        const superManTextures: PIXI.Texture[] =
+            ["llama1.png", "llama2.png", "llama3.png", "llama4.png", "llama5.png"]
+                .map(x => PIXI.Texture.fromFrame(x))
+
+        this.sprite = new PIXI.extras.AnimatedSprite(superManTextures);
+        this.sprite.scale.x = 2;
+        this.sprite.scale.y = 2;
+        this.sprite.x = this.app.screen.width / 2;
+        this.sprite.y = this.app.screen.height - this.sprite.height;
+        this.sprite.animationSpeed = 0.1;
+        this.sprite.play();
+        
+        this.app.stage.addChild(this.sprite)
+        this.app.ticker.add(this.movingHandler.bind(this))
+        this.app.ticker.add(this.flipDirectionHandler.bind(this))
         return this;
     }
 
@@ -31,16 +47,13 @@ export class SuperMan {
     }
 
     protected flipDirectionHandler(): void {
-        
-        if (this.sprite.x + (this.sprite.width / 2) > this.app.screen.width) {
+        if (this.sprite.x + this.sprite.width  > this.app.screen.width) {
             this.currentDirection = SuperManDirection.left;
-            this.sprite.scale.x *= -1;
+            // this.sprite.scale.x *= -1;
         } else if (this.sprite.x < 0) {
             this.currentDirection = SuperManDirection.right;
-            this.sprite.scale.x *= -1;
+            // this.sprite.scale.x *= -1;
         }
-
-
     }
 
     goRight(): void {
@@ -49,6 +62,12 @@ export class SuperMan {
 
     goLeft(): void {
         this.sprite.position.x -= this.moveSpeed;
+    }
+
+    turnDirection(): void {
+        this.currentDirection = this.currentDirection === SuperManDirection.left ?
+            SuperManDirection.right : SuperManDirection.left;
+        // this.sprite.scale.x *= -1;
     }
 
 }
