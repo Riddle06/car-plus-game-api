4e453e12e7f3afa4ee95fd57b006b9f54e593fee
david.jian <clipwww@gmail.com>
2018-11-21 23:08:34 +0800

    feat: [接接樂] 超人改用雪碧動圖 ＆ 圖片可以轉向

--- src-client/games/catch.game/super-man.ts
+++ src-client/games/catch.game/super-man.ts
@@ -5,39 +5,46 @@ export class SuperMan {
         x: number
         y: number
     } = null
-    private moveSpeed: number = 3
+    private moveSpeed: number = 3;
+    private animationSpeed: number  = 0.1;
     private app: PIXI.Application = null
     private currentDirection: SuperManDirection = SuperManDirection.right;
-    
+    private rightSprite: PIXI.extras.AnimatedSprite = null;
+    private leftSprite: PIXI.extras.AnimatedSprite = null;
+
     public sprite: PIXI.Graphics = null;
-    public superManLeft: PIXI.Sprite = null
-    public superManRight: PIXI.Sprite = null
+    
     
     constructor(app: PIXI.Application) {
         this.app = app;
     }
 
     async init(): Promise<this> {
-        // const loderRes = await loaderHandler('/static/images/llama/llama.json');
-        // const superManTextures: PIXI.Texture[] =
-        //     ["llama1.png", "llama2.png", "llama3.png", "llama4.png", "llama5.png"]
-        //         .map(x => PIXI.Texture.fromFrame(x))
-        await loaderHandler('super-man-left', '/static/images/img-superman-left.png');
-        await loaderHandler('super-man-right', '/static/images/img-superman-right.png');
+        await loaderHandler('super-man01', '/static/images/img-superman01/img-superman01.json');
+        const superManFrames: PIXI.Texture[] =
+            ["img-superman010.png", "img-superman011.png", "img-superman012.png"]
+                .map(x => PIXI.Texture.fromFrame(x))
 
-        this.superManLeft = new PIXI.Sprite(PIXI.loader.resources['super-man-left'].texture);
-        this.superManRight = new PIXI.Sprite(PIXI.loader.resources['super-man-right'].texture);
-        this.superManLeft.visible = false;
-
+        
         this.sprite = new PIXI.Graphics();
-        this.sprite.addChild(this.superManLeft);
-        this.sprite.addChild(this.superManRight);
-        this.sprite.height = (this.sprite.height / this.sprite.width) * (this.app.screen.width / 5);
+        this.rightSprite = new PIXI.extras.AnimatedSprite(superManFrames);
+        this.leftSprite = new PIXI.extras.AnimatedSprite(superManFrames);
+        this.sprite.addChild(this.leftSprite);
+        this.sprite.addChild(this.rightSprite);
+        this.sprite.height = (this.rightSprite.height / this.rightSprite.width) * (this.app.screen.width / 5);
         this.sprite.width = this.app.screen.width / 5;
+
+        // 轉向製作向左邊的圖
+        this.leftSprite.scale.x *= -1;
+        this.leftSprite.x = this.leftSprite.width;
+
+        this.rightSprite.animationSpeed = this.animationSpeed;
+        this.leftSprite.animationSpeed = this.animationSpeed;
+        this.leftSprite.visible = false;
+
+        
         this.sprite.x = this.app.screen.width / 2 - (this.sprite.width / 2);
         this.sprite.y = this.app.screen.height - this.sprite.height - 30;
-        // this.sprite.animationSpeed = 0.1;
-        
         
         this.app.stage.addChild(this.sprite)
 
@@ -47,21 +54,19 @@ export class SuperMan {
     protected movingHandler(deltaTime: number): void {
         if (this.sprite.x + this.sprite.width  > this.app.screen.width) {
             this.currentDirection = SuperManDirection.left;
-            // this.sprite.scale.x *= -1;
         } else if (this.sprite.x < 0) {
             this.currentDirection = SuperManDirection.right;
-            // this.sprite.scale.x *= -1;
         }
 
         switch (this.currentDirection) {
             case SuperManDirection.right:
-                this.superManLeft.visible = false;
-                this.superManRight.visible = true;
+                this.rightSprite.visible = true;    
+                this.leftSprite.visible = false;
                 this.goRight();
                 break;
             case SuperManDirection.left:
-                this.superManLeft.visible = true;
-                this.superManRight.visible = false;
+                this.rightSprite.visible = false;    
+                this.leftSprite.visible = true;
                 this.goLeft();
                 break;
         }
@@ -69,12 +74,14 @@ export class SuperMan {
 
 
     start() {
-        // this.sprite.play();
+        this.rightSprite.play();
+        this.leftSprite.play();
         this.app.ticker.add(this.movingHandler, this);
     }
 
     end() {
-        // this.sprite.stop();
+        this.rightSprite.stop();
+        this.leftSprite.stop();
         this.app.ticker.remove(this.movingHandler, this);
     }
 
@@ -89,7 +96,6 @@ export class SuperMan {
     turnDirection(): void {
         this.currentDirection = this.currentDirection === SuperManDirection.left ?
             SuperManDirection.right : SuperManDirection.left;
-        // this.sprite.scale.x *= -1;
     }
 
 }
