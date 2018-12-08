afdf9d243a28630271b223469187b2823fb82c9c
Rex Li <rex@myre.life>
2018-12-09 00:16:36 +0800

    feat: 問與答，若開啟另外一個答案要將原有的收合

--- src-client/entry/question.ts
+++ src-client/entry/question.ts
@@ -3,28 +3,38 @@ import { BasePage } from "./base.page";
 class QuestionPage extends BasePage {
     private $info: JQuery<HTMLElement>;
 
+    private currentId: string;
+
     domEventBinding() {
         const _this = this;
 
         this.$info = this.$("#js-info");
-
         this.$(".js-question").click((e) => {
+
+            if (this.currentId) {
+                this.$(`#js-answer-${this.currentId}`).slideToggle(400);
+            }
             const id = this.$(e.currentTarget).attr('data-index');
-            this.$(`#js-answer-${id}`).slideToggle(400);
+
+            if (this.currentId !== id) {
+                this.$(`#js-answer-${id}`).slideToggle(400);
+                this.currentId = id;
+            }
+
         })
-    } 
+    }
     didMount() {
         this.getMemberProfile();
     }
-    
+
 
     async getMemberProfile(): Promise<void> {
-        
+
         const profileRet = await this.webSvc.member.getProfile()
 
         const { gamePoint, carPlusPoint, currentRoleGameItem } = profileRet.item;
         const { spriteFolderPath } = currentRoleGameItem;
-    
+
         this.$info.find("#js-gamePoint").text(gamePoint);
         this.$info.find("#js-carPlusPoint").text(carPlusPoint);
 
