0f87fd1b57be68db32a7a65a7203ccc170463200
david.jian <clipwww@gmail.com>
2018-12-12 22:49:37 +0800

    feat: 串接會員總覽

--- src-client/entry-admin/base.page.ts
+++ src-client/entry-admin/base.page.ts
@@ -1,14 +1,61 @@
 import { adminSvc } from "../web-services";
+import { GameItemType } from '@view-models/game.vm';
 import * as Vue from 'vue/dist/vue.common'
 import ElementUI from 'element-ui';
+import * as cookie from "js-cookie";
 import '../assets/scss/admin/index.scss';
 
 export abstract class BasePage {
     protected adminSvc = adminSvc;
+    protected loadingInstance = null;
+
+    protected messageBoxOption = {
+      dangerouslyUseHTMLString: true,
+      center: true,
+      showConfirmButton: true,
+      showCancelButton: true, 
+      confirmButtonText: '確定',
+      cancelButtonText: '取消'
+    }
     
     constructor() {
       Vue.use(ElementUI);
       this.vueInit();
+    }
+
+    getAdminToken(): string {
+      const token = cookie.get('admin');
+
+      return token;
+  }
+
+    getGameItemTypeName(type: number): Number | String {
+      switch(type) {
+        case GameItemType.role:
+          return '角色';
+        case GameItemType.tool:
+          return '道具'
+        case GameItemType.gamePoint:
+          return '超人幣'
+        case GameItemType.carPlusPoint:
+          return '格上紅利'
+        default:
+          return type;
+      }
+    }
+    
+    openLoader() {
+      this.loadingInstance = ElementUI.Loading.service({
+        fullscreen: true,
+        background: 'rgba(0,0,0, .7)',
+        spinner: 'el-icon-loading',
+      })
+    }
+
+    closeLoader() {
+      Vue.nextTick(() => {
+        this.loadingInstance.close();
+      })
     }
 
     abstract vueInit()
--- src-client/entry-admin/members.ts
+++ src-client/entry-admin/members.ts
@@ -9,6 +9,8 @@ class MembersPage extends BasePage {
       el: '#app',
       data() {
         return {
+          memberId: '',
+          member: {},
           members: [
             { id: 1, name: '無名', level: 10, coin: 100, }
           ],
@@ -16,6 +18,27 @@ class MembersPage extends BasePage {
             index: 1,
             size: 10,
             dataAmount: 0,
+          },
+
+          blockForm: {
+            memberId: '',
+            reason: '',
+            adminUserName: '',
+          },
+
+
+          isDialogOpen: false,
+          isBlockDialogOpen: false,
+        }
+      },
+      watch: {
+        isDialogOpen(bool) {
+          if (!bool) this.member = {};
+        },
+        isBlockDialogOpen(bool) {
+          if (!bool) {
+            this.member = {};
+            this.$refs.form.resetFields();
           }
         }
       },
@@ -26,10 +49,59 @@ class MembersPage extends BasePage {
           const ret = await _this.adminSvc.adminMember.getMembers({
             pageIndex: this.page.index,
             pageSize: this.page.size
-          });
+          }, { memberId: this.memberId });
+
           this.members = ret.items;
           this.page = { ...this.page, ...ret.page };
         },
+
+        async openMemberDetail(member) {
+          _this.openLoader();
+          const ret = await _this.adminSvc.adminMember.getMemberDetail(member.id);
+          _this.closeLoader();
+          console.log(ret)
+
+          this.member = ret.item;
+          this.isDialogOpen = true;
+        },
+
+        openBlockDialog(member) {
+          this.member = member;
+          this.blockForm.memberId = member.id;
+          this.isBlockDialogOpen = true;
+        },
+
+        async addBlockMember() {
+          const isValid = await this.$refs.form.validate().catch(() => false);
+          if (!isValid) {
+            return;
+          }
+          const ret = await _this.adminSvc.adminMember.blockMember(this.blockForm);
+          if (!ret.success) {
+            this.$notify({
+              type: 'error',
+              title: '封鎖失敗',
+              message: ret.message
+            });
+            return;
+          }
+          this.$notify({
+            type: 'success',
+            title: '封鎖成功'
+          });
+          this.isBlockDialogOpen = false;
+        },
+
+        exportExcel() {
+          window.open(_this.adminSvc.adminExport.getExportMemberWithGameItems({
+            token: _this.getAdminToken(),
+          }))
+        },
+
+        getGameItemTypeName(type) {
+          return _this.getGameItemTypeName(type);
+        },
+
         handlePageChange(index) {
           console.log(index);
           this.page.index = index;
