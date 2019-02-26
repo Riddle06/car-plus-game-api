fc0412cced2800325ae5ace22dc20c3d48e490bf
Rex Li <rex@myre.life>
2019-02-26 22:13:29 +0800

    feat: 新增 event trigger service

---
+++ src/services/event.trigger.svc/index.ts
@@ -0,0 +1,13 @@
+import { QueryRunner } from 'typeorm';
+import { CarPlusMemberInformation } from "@view-models/car-plus.vm";
+import { Result } from "@view-models/common.vm";
+import { dbProvider } from "@utilities";
+import { variableSvc } from '@services/variable.svc';
+
+class EventTrigger {
+
+  
+}
+
+
+export const eventTrigger = new EventTrigger();
